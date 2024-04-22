4f6e6b6a658fd13e2fc0423b4da3e25ba4e2bc36
harishraisinghani <harishraisinghani@users.noreply.github.com>
2024-04-22 03:59:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/tx/[chain]/[tx_hash]/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { Chain } from "@covalenthq/client-sdk"
+import { TransactionReceiptView } from "@covalenthq/goldrush-kit"
+import { Flex } from "@radix-ui/themes"
+
+import { Button } from "@/components/ui/button"
+
+export default function TX({
+  params,
+}: {
+  params: { chain: Chain; tx_hash: string }
+}) {
+  const router = useRouter()
+
+  return (
+    <div className="w-full flex items-center flex-col gap-4">
+      <div className="w-full min-h-[40rem] relative md:w-[50rem] md:flex-row">
+        <TransactionReceiptView
+          chain_name={params.chain}
+          tx_hash={params.tx_hash}
+        />
+      </div>
+
+      <Flex
+        className="w-full"
+        onClick={() => {
+          router.back()
+        }}
+      >
+        <Button>Back</Button>
+      </Flex>
+    </div>
+  )
+}
